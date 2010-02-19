04285daec1d5ea5b169ecf5b9395c5786e283e16
Nik.Cimino@gmail.com <Nik.Cimino@gmail.com@aa718914-e166-11de-8dcd-9b777bee35ae>
2010-02-19 08:10:07 +0000

    The HTML DOM has started to be constructed. HTML,HTML_: Head,Title,ShortcutIcon,Body,Div,Element,Link are working.  They may need to be expanded, but currently have sufficient structure for use.

--- include/HTMLDiv.php
+++ include/HTMLDiv.php
@@ -1,9 +1,7 @@
 <?php
-class HTMLDiv {
-  public $HTMLElement;
-  function __construct($HTMLDocument,$parent) {
-    $this->HTMLElement = $HTMLDocument->createElement( 'div', '' );
-    $parent->HTMLElement->appendChild( $this->HTMLElement );
+class HTMLDiv extends HTMLElement {
+  function __construct($HTMLDocument,$parentElement) {
+    parent::__construct($HTMLDocument->HTMLDocument,$parentElement->HTMLElement,'div','');
   }
 }
 ?>
--- include/HTMLElement.php
+++ include/HTMLElement.php
@@ -1,11 +1,30 @@
 <?php
 class HTMLElement {
-  public $HTMLDocument;
-  function __construct($HTMLDocument) {
-    $this->HTMLDocument = $HTMLDocument;
+  public $HTMLElement;
+  public $tagName;
+  public $innerHTML;
+  function __construct($HTMLDocument,$parentElement,$tagName,$innerHTML=NULL) {
+    $this->tagName=$tagName;
+    $this->innerHTML=$innerHTML;
+    $this->createElement($HTMLDocument,$tagName,$innerHTML);
+    $this->appendChild($parentElement);
   }
-  function appendChild($HTMLElement) {
-    $this->HTMLDocument->appendChild( $HTMLElement );
+  function appendChild($parentElement) {
+    $parentElement->appendChild( $this->HTMLElement );
+  }
+  function createElement($HTMLDocument,$tagName,$innerHTML=NULL) {
+    if ($innerHTML==NULL) {
+      $this->HTMLElement = $HTMLDocument->createElement( $tagName );
+    } else {
+      $this->HTMLElement = $HTMLDocument->createElement( $tagName, $innerHTML );
+    }
+  }
+  function createTextNode($HTMLDocument,$content) {
+    $this->innerHTML = $HTMLDocument->createTextNode( $content );
+    $this->HTMLElement->appendChild($this->innerHTML);
+  }
+  function setAttribute($name,$value) {
+    $this->HTMLElement->setAttribute( $name, $value );
   }
 }
 ?>
