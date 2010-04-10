5f0707b45ad4875503340f1e63279e23114d3c8c
Nik.Cimino@gmail.com <Nik.Cimino@gmail.com@aa718914-e166-11de-8dcd-9b777bee35ae>
2010-04-10 23:21:34 +0000

    On Account Management page inactive group accounts are still showing.  Changed Ajax calls so they are now jQuery.  The Ajax is working in IE 6/7/8, but the drag and drop relative offset call is causing a nother problem.

--- js/main.js
+++ js/main.js
@@ -90,53 +90,18 @@ function sendGetRequest(url, focus_id) {
 }
 
 function sendPostRequest(url, content_id, post_data, focus_id, after_load) {
-	var req = createXMLHTTPObject();
-	if (!req) {
-		document.getElementById(content_id).innerHTML = "An error has occured.";
-		return;
-	}
-	req.open("POST", url, true);
-	req.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	req.onreadystatechange = function() {
-		if (req.status != 200 && req.status != 304) {
-			document.getElementById(content_id).innerHTML = "An error has occured.";
-			return;
-		} else if (req.readyState == 4) {
-			document.getElementById(content_id).innerHTML = req.responseText;
-			QaHideMsgs();
-			if (focus_id)
-				focus(focus_id);
-			if (after_load)
-				$(after_load);
-			return;
-		}
-	};
-	document.getElementById(content_id).innerHTML = "Loading...";
-	req.send(post_data);
-}
-
-function XMLHttpFactories() {
-	return [ function() {
-		return new XMLHttpRequest();
-	}, function() {
-		return new ActiveXObject("Msxml2.XMLHTTP");
-	}, function() {
-		return new ActiveXObject("Msxml3.XMLHTTP");
-	}, function() {
-		return new ActiveXObject("Microsoft.XMLHTTP");
-	} ];
-}
-
-function createXMLHTTPObject() {
-	var xmlhttp = false;
-	var factories = XMLHttpFactories();
-	for ( var i = 0; i < factories.length; i++) {
-		try {
-			xmlhttp = factories[i]();
-		} catch (e) {
-			continue;
-		}
-		break;
-	}
-	return xmlhttp;
+	$.ajax({
+		   type: "POST",
+		   url: url,
+		   data: post_data,
+		   beforeSend: function(){
+		document.getElementById(content_id).innerHTML = "Loading...";
+		},
+		   success: function(msg){
+			document.getElementById(content_id).innerHTML = msg;
+			   QaHideMsgs();
+				if (focus_id) focus(focus_id);
+				if (after_load) $(after_load);
+			   }
+		 });
 }
