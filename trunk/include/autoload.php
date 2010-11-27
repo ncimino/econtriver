4fc1263bb2539e3adb9dab87fa06d25ef24608fd
nik.cimino@gmail.com <nik.cimino@gmail.com@aa718914-e166-11de-8dcd-9b777bee35ae>
2010-11-27 03:50:09 +0000

    Created the qa includes directory and moved all of the AjaxQa* classes into this directory.

--- trunk/include/autoload.php
+++ trunk/include/autoload.php
@@ -2,7 +2,9 @@
 function __autoload($class_name) {
 	if (preg_match("/^HTML([a-zA-Z])+$/",$class_name)) {
 		require_once 'html/' . $class_name . '.php';
-	} else {
+	} elseif (preg_match("/^AjaxQa([a-zA-Z])+$/",$class_name)) {
+		require_once 'qa/' . $class_name . '.php';
+	}else {
 		require_once $class_name . '.php';
 	}
 }
