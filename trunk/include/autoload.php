6c8b0aad08ff7fbca2cb6e1c8e5147ca04f2f1af
Nik.Cimino@gmail.com <Nik.Cimino@gmail.com@aa718914-e166-11de-8dcd-9b777bee35ae>
2010-12-04 02:59:29 +0000

    Beginning to switch over to a system that uses underscores to designate folders in the includes.

--- trunk/include/autoload.php
+++ trunk/include/autoload.php
@@ -1,5 +1,5 @@
 <?php
 function __autoload($class_name) {
-	require(str_replace('_', DIRECTORY_SEPARATOR, $class) . '.php');
+	require(str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php');
 }
 ?>
